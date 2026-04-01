19c8849b38a8660522ea8e6cc20e92dd0cc46427
Blues24 <lukmanaffandi900@gmail.com>
2026-04-01 13:05:23 +0700

    Set DB_PASS to an empty string in bootstrap.php
    Change DB_PASS environment variable to an empty string.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@
 putenv('DB_HOST=127.0.0.1');
 putenv('DB_NAME=batik_test');
 putenv('DB_USER=root');
-putenv('DB_PASS=root');
+putenv('DB_PASS=');
 putenv('DB_PORT=3306');
 
 require_once __DIR__ . '/../backend/config/database.php';
